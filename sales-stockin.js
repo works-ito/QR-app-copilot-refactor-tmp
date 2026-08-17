18078040e587ed4c0b52b5cefe2aba445547940d
works-ito <h.ito@works-rental.co.jp>
2026-08-18 06:36:23 +0900

    Load three-pixel master spacing v61

--- sales-stockin.js
+++ sales-stockin.js
@@ -1,4 +1,4 @@
-/* 販売品入庫受付 v60 bootstrap */
+/* 販売品入庫受付 v61 bootstrap */
 (function() {
   function loadScript(src) {
     return new Promise(function(resolve, reject) {
@@ -32,7 +32,7 @@
       return loadScript("./irregular-master-picker-dev.js?v=58");
     })
     .then(function() {
-      return loadScript("./irregular-category-ui-tuning-dev.js?v=60");
+      return loadScript("./irregular-category-ui-tuning-dev.js?v=61");
     })
     .then(function() {
       return loadScript("./irregular-simple-id-alias-dev.js?v=42");
