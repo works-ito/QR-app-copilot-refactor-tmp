77b792c9a2cc76876892eebaa016184c55816fba
works-ito <h.ito@works-rental.co.jp>
2026-08-18 01:27:53 +0900

    Load compact picker UI v55

--- sales-stockin.js
+++ sales-stockin.js
@@ -1,4 +1,4 @@
-/* 販売品入庫受付 v54 bootstrap */
+/* 販売品入庫受付 v55 bootstrap */
 (function() {
   function loadScript(src) {
     return new Promise(function(resolve, reject) {
@@ -29,10 +29,10 @@
       return loadScript("./compact-scanner-dev.js?v=53");
     })
     .then(function() {
-      return loadScript("./irregular-master-picker-dev.js?v=50");
+      return loadScript("./irregular-master-picker-dev.js?v=55");
     })
     .then(function() {
-      return loadScript("./irregular-category-ui-tuning-dev.js?v=54");
+      return loadScript("./irregular-category-ui-tuning-dev.js?v=55");
     })
     .then(function() {
       return loadScript("./irregular-simple-id-alias-dev.js?v=42");
@@ -44,7 +44,7 @@
       return loadScript("./irregular-registration-guard-dev.js?v=43");
     })
     .then(function() {
-      return loadScript("./irregular-quantity-flow-dev.js?v=44");
+      return loadScript("./irregular-quantity-flow-dev.js?v=55");
     })
     .then(function() {
       return loadScript("./mode-description-hint-dev.js?v=37");
