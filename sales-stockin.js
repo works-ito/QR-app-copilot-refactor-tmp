ca9137d050e8e43cfa84f977a4965424858c1ef0
works-ito <h.ito@works-rental.co.jp>
2026-08-17 14:33:22 +0900

    Bump dev scanner cache version to v46

--- sales-stockin.js
+++ sales-stockin.js
@@ -1,4 +1,4 @@
-/* 販売品入庫受付 v45 bootstrap */
+/* 販売品入庫受付 v46 bootstrap */
 (function() {
   function loadScript(src) {
     return new Promise(function(resolve, reject) {
@@ -20,7 +20,7 @@
       return loadScript("./sales-stockin-guards.js?v=33");
     })
     .then(function() {
-      return loadScript("./compact-scanner-dev.js?v=45");
+      return loadScript("./compact-scanner-dev.js?v=46");
     })
     .then(function() {
       return loadScript("./irregular-master-picker-dev.js?v=41");
