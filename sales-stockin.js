58bfc75676a0245ce7da2c1b99eaa93839d0c8b3
works-ito <h.ito@works-rental.co.jp>
2026-08-19 12:10:05 +0900

    Load quantity transfer development module

--- sales-stockin.js
+++ sales-stockin.js
@@ -1,4 +1,4 @@
-/* 販売品入庫受付 v72 bootstrap */
+/* 販売品入庫受付 v73 bootstrap */
 (function() {
   function loadScript(src) {
     return new Promise(function(resolve, reject) {
@@ -51,6 +51,9 @@
     })
     .then(function() {
       return loadScript("./irregular-master-send-bridge-dev.js?v=71");
+    })
+    .then(function() {
+      return loadScript("./quantity-transfer-dev.js?v=73");
     })
     .then(function() {
       return loadScript("./mode-description-hint-dev.js?v=37");
