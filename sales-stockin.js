e67bb325baae6d8f386c421e7a3b2af72da48a07
works-ito <h.ito@works-rental.co.jp>
2026-08-19 14:14:50 +0900

    Load quantity transfer bridge fix v76

--- sales-stockin.js
+++ sales-stockin.js
@@ -1,4 +1,4 @@
-/* 販売品入庫受付 v75 bootstrap */
+/* 販売品入庫受付 v76 bootstrap */
 (function() {
   function loadScript(src) {
     return new Promise(function(resolve, reject) {
@@ -50,7 +50,7 @@
       return loadScript("./irregular-quantity-flow-dev.js?v=55");
     })
     .then(function() {
-      return loadScript("./irregular-master-send-bridge-dev.js?v=71");
+      return loadScript("./irregular-master-send-bridge-dev.js?v=72");
     })
     .then(function() {
       return loadScript("./quantity-transfer-dev.js?v=75");
